fd02ad1d0f4b5a2cbfdc6a4cb0b23217474ecdf3
BrunnaMaiaradaSilva <silva.brunnam@gmail.com>
2020-01-06 11:15:40 -0200

    Update

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,4 +1,24 @@
 class Survivor < ApplicationRecord
     has_one :inventory #relação do inventário com o survivor
-    attribute :ativa, Infected.new
+    has_many :flags
+
+end
+
+ActionController::Parameters.permit_all_parameters = true
+
+	validates :name, :age, :gender, :lat, :long, presence: true
+
+	scope :infected, -> { where("infected_reports >= 3") }
+	scope :not_infected, -> { where("infected_reports < 3") }
+
+	def initializer
+	end
+
+	def infected?
+		if self.infected_reports >= 3
+			true
+		else
+			false
+		end
+	end
 end
